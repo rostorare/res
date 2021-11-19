d3e53b4ffe417a790ef65c7705c2ad6d81e78dfd
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2021-11-19 10:13:21 +0100

    first commit

---
+++ src/main/java/htw/reservierer/ReserviererApplication.java
@@ -0,0 +1,13 @@
+package htw.reservierer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserviererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserviererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/reservierer/ReserviererApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.reservierer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserviererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
