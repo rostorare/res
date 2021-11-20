80774ac5ca6e4d7c9e20d0dade3ccbe65b75de0c
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2021-11-20 21:39:24 +0100

    added jar props & lala

--- src/main/java/htw/reservierer/GreetingController.java
+++ src/main/java/htw/reservierer/GreetingController.java
@@ -18,4 +18,9 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+    @GetMapping("/")
+    public Greeting greetingx(@RequestParam(value ="name",defaultValue = "World")String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+
 }
